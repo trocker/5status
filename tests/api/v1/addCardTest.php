31ab2e9a526fd8e7bc9fac6f1badf2e689d6e33b
Srinivasa Teja <nsteja1@gmail.com>
2015-09-28 22:34:36 -0400

    BugFix #1 - DB Wrapper adjustment

--- tests/api/v1/addCardTest.php
+++ tests/api/v1/addCardTest.php
@@ -2,7 +2,7 @@
 
 //Check the database from 
 
-include_once dirname(__FILE__).'../src/lib/wrappers/db.php';
+include_once dirname(__FILE__).'/../../../src/lib/wrappers/db.php';
 $dbconn = new DBConn($dbhost, $dbuser, $dbpassword, $dbname);
 $query = "SELECT * FROM cards ORDER BY id DESC LIMIT 0,1";
 $result = $dbconn->execute($query);
