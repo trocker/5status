5d261498b4b36841612979ff645ec6339f39c6ad
Srinivasa Teja <nsteja1@gmail.com>
2015-10-25 00:20:08 -0400

    Added config file to place the S3 Access key in

--- src/lib/config.php
+++ src/lib/config.php
@@ -13,3 +13,4 @@ $dbname = "5status";
 $mandrill_key = "QMtdRBE-xHE4dIVTKhy_Tw";
 $salt = "$(*(&^1";
 $default_image = "http://societyofillustratorssandiego.org/wp-content/uploads/2015/03/male-placeholder.gif";
+//Place the S3 Upload Key here
