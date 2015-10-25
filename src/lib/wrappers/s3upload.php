1310c08d3fe5034428b37803cacc9f0b76eb6618
Srinivasa Teja <nsteja1@gmail.com>
2015-10-25 00:19:09 -0400

    Frameworks for Invite to Card script for Bhagya and S3Upload library for Sindhu

---
+++ src/lib/wrappers/s3upload.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+*	Library to do S3 Upload a file
+*/
+
+
+class S3Upload{
+
+	//Take the key from the config file
+
+	//Function to upload the file, takes parameters - local_file_location, file_container, bucket, metadata. Return S3 uploaded URL.
+
+}
+
+
+/* use src/api/v1/test.php to write a test script and run it with php */
