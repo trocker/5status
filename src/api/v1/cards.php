e7461c9b3f743d068078044d0b2bebf2df19c96d
Srinivasa Teja <nsteja1@gmail.com>
2015-11-26 14:07:49 -0500

    Correct formation of cards now with card sharers being shown

--- src/api/v1/cards.php
+++ src/api/v1/cards.php
@@ -30,18 +30,27 @@ if($authObject->isAuthenticated()){
 	 * 	Continue assuming user is Authenticated 
 	 */
 	$dbconn = new DBConn($dbhost, $dbuser, $dbpassword, $dbname);
-	$query = "SELECT * FROM cards WHERE owner_id='".$input['user_id']."' ORDER BY creation_date DESC";
+	$query = "SELECT cards.*, accounts.name, accounts.picture FROM cards INNER JOIN accounts WHERE owner_id='".$input['user_id']."' AND accounts.user_id = cards.owner_id ORDER BY cards.creation_date DESC";
+	//echo $query;
 	$result = $dbconn->execute($query);
 	if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc()) {
+
+			//get all the card sharers for this card
+			$result_cardsharers = $dbconn->execute("SELECT * FROM card_sharers INNER JOIN accounts WHERE card_id = '".$row['id']."' AND accounts.user_id = card_sharers.user_id");
+			$card_sharers = array();
+			while($row_cardsharer = $result_cardsharers->fetch_assoc()){
+				$card_sharers[] = array("user_id" => $row_cardsharer['user_id'], "picture" => $row_cardsharer['picture'], "name" => $row_cardsharer['name']);
+			}
+
 			//get the latest comment
-			$result_comment = $dbconn->execute("SELECT * FROM comments INNER JOIN accounts WHERE card_id='".$row['id']."'  AND accounts.user_id = comments.user_id ORDER BY comments.creation_date DESC");
+			$result_comment = $dbconn->execute("SELECT comments.*, accounts.* FROM comments INNER JOIN accounts WHERE comments.card_id='".$row['id']."'  AND accounts.user_id = comments.user_id ORDER BY comments.creation_date DESC");
 			$result_comment_latest = $result_comment->fetch_assoc();
 			if(! $result_comment->num_rows){
-				$result_comment_latest = array("created_on" => $row['creation_date'], "comment" => "created the card.", "user_id" => $row['owner_id']);
+				$result_comment_latest = array("created_on" => $row['creation_date'], "comment" => "created the card.", "user_id" => $row['owner_id'], "picture" => $row['picture'], "name" => $row['name']);
 			}
 			//form a response
-			$response['cards'][] = array("card_title" => $row['card_title'], "card_id" => $row['id'], "created_on" => $row['creation_date'], "owner_id" => $row['owner_id'], "status" => $row['card_status'],"number_of_comments"=>$result_comment->num_rows , "comments" => array(array("created_on" => $result_comment_latest['creation_date'], "comment" => $result_comment_latest['comment'], "user_id" => $result_comment_latest['user_id'],  "user_id" => $result_comment_latest['picture'])));
+			$response['cards'][] = array("card_title" => $row['card_title'], "card_id" => $row['id'], "created_on" => $row['creation_date'], "owner_id" => $row['owner_id'], "status" => $row['card_status'],"number_of_comments"=>$result_comment->num_rows ,"card_sharers" => $card_sharers , "comments" => array(array("created_on" => $result_comment_latest['creation_date'], "comment" => $result_comment_latest['comment'], "user_id" => $result_comment_latest['user_id'],  "picture" => $result_comment_latest['picture'], "name" => $result_comment_latest['name'])));
 		}
 	}
 	$dbconn->close();
