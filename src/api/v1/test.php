4c943238ee63f1edc5973d2db2cfe34ddea821b6
Srinivasa Teja <nsteja1@gmail.com>
2015-11-27 13:43:48 -0500

    Queue Wrapper implemented. Test Script added as well.

--- src/api/v1/test.php
+++ src/api/v1/test.php
@@ -1,11 +1,22 @@
 <?php
 
-include_once dirname(__FILE__).'/../../lib/wrappers/notifier.php';
+include_once dirname(__FILE__).'/../../lib/wrappers/queue.php';
+include_once dirname(__FILE__).'/../../lib/config.php';
 
-$notifierObject = new Notifier();
 
-$to = "nsteja1@gmail.com";
-$from = "contact@5status.com";
-$subject = "5status Test Mail";
-$body = "Hello World!";
-print_r($notifierObject->sendEmail($to, $from, $subject, $body));
+$queueclient = new QueueClient($queue_host, $queue_port);
+
+
+$queueclient->enqueue("reverse", serialize("hello world!"), 1);
+
+
+function run($job){
+	$workload = $job->workload();
+	echo strrev(unserialize($workload));
+	return strrev(unserialize($workload));
+}
+
+$queueworker = new QueueWorker($queue_host, $queue_port, "reverse");
+
+$queueworker->dequeue();
+
---
+++ src/lib/wrappers/queue.php
@@ -0,0 +1,54 @@
+<?php
+
+
+/**
+* Queue wrapper - Classes QueueClient QueueWorker 
+*/
+
+class QueueClient {
+	
+	private $gmclient= NULL;
+
+	function __construct($queue_host, $queue_port) {
+		$this->gmclient = new GearmanClient();
+		$this->gmclient->addServer($queue_host, $queue_port);
+	}
+
+	function enqueue($job_type, $workload, $priority) {
+		//TODO: depending upon $priority number do high, low or normal
+		$this->gmclient->doBackground($job_type, $workload);
+	}
+
+	function enqueueSync($job_type, $workload, $priority) {
+		return $this->gmclient->do($job_type, $workload);
+	}
+}
+
+
+class QueueWorker {
+	
+	private $gmworker= NULL;
+	private $worker_type = NULL;
+
+
+	function __construct($queue_host, $queue_port, $worker_type) {
+		$this->gmworker = new GearmanWorker();
+		$this->gmworker->addServer($queue_host, $queue_port);
+		$this->gmworker->worker_type = $worker_type;
+		//Call when the work_run_time function when appropriate
+		$this->gmworker->addFunction($worker_type, 'run');
+
+	}
+
+	function dequeue() {
+		if($this->gmworker->work()){
+			if ($this->gmworker->returnCode() != GEARMAN_SUCCESS) {
+				//TODO: put logger here
+			    echo "Worker fault!";
+			  }
+			 
+		}
+
+	}
+}
+
